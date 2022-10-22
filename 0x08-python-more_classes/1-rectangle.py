edc45e23016846888fc305c4c59bec9ee6531235
Okedamola01 <okedamola01@gmail.com>
2022-10-22 10:56:34 +0000

    1 rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python3
 """
-Defines a class rectangle
+This module defines the a Rectangle Object.
 """
 
 
 class Rectangle:
-    """Representation of a rectangle"""
+    """Retangle object with getter and setters
+    """
     def __init__(self, width=0, height=0):
-        """Initialized the rectangle"""
-        self.height = height
         self.width = width
+        self.height = height
 
-        @property
-        def width(self):
-            """getter for the private instance attibute width"""
-            return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """setter for the private instance attribute width"""
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
 
-        @property
-        def height(self):
-            """getter for te private instance attribute width"""
-            return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """setter for the height value"""
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if height < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
